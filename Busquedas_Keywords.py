8ad1c3e145bf4625010f30eb4c9860b054ad8652
FranJ <Fr4n.mlg@gmail.com>
2023-07-26 17:46:21 +0200

    Añadido ejemplo de busqueda concreta por key word

--- Busquedas_Keywords.py
+++ Busquedas_Keywords.py
@@ -14,8 +14,16 @@ results = soup.find(id="ResultsContainer")
 # Busco por nombre de clase y guardo en un objeto que contendrá cosas
 job_elements = results.find_all("div", class_="card-content")
 
-# Desgloso ese objeto en sus contenedores donde esta el texto con la info y la muestro en pantalla
-for job_element in job_elements:
+# Puedo hacer busquedas concretas con key words. En este caso veo la info que contenga "Python"
+python_jobs = results.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
+
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
+for job_element in python_job_elements:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
